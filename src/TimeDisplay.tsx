acd5dc8afd2c413b32e76186a8efb2c033925c38
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-06-28 21:16:48 -0700

    Simplify repetitive code

--- src/TimeDisplay.tsx
+++ src/TimeDisplay.tsx
@@ -21,34 +21,20 @@ function TimeDisplay({ secondsRemaining }: Props) {
         </span>
       </div>
     );
-  } else if (secondsRemaining >= 60) {
-    const minutesRemaining = 1;
-    const secondsRemainingAfterMinutes =
-      Math.abs(secondsRemaining) - minutesRemaining * 60;
-    return (
-      <div className="time-countdown">
-        <span className="time-limited">
-          <span className="time-number">1</span>{" "}
-          <span className="time-caption">{MINUTES_CAPTION}</span>{" "}
-          <span className="time-number">{secondsRemainingAfterMinutes}</span>{" "}
-          <span className="time-caption">{SECONDS_CAPTION}</span>
-        </span>
-      </div>
-    );
   } else if (secondsRemaining >= 0) {
+    const minutesRemaining = Math.floor(secondsRemaining / 60);
+    const secondsRemainingAfterMinutes =
+      secondsRemaining - minutesRemaining * 60;
     return (
       <div className="time-countdown">
         <span className="time-limited">
-          <span className="time-number">{secondsRemaining}</span>{" "}
-          <span className="time-caption">{SECONDS_CAPTION}</span>
-        </span>
-      </div>
-    );
-  } else if (secondsRemaining > -60) {
-    return (
-      <div className="time-countdown">
-        <span className="time-over">
-          <span className="time-number">{secondsRemaining}</span>{" "}
+          {minutesRemaining > 0 && (
+            <>
+              <span className="time-number">{minutesRemaining}</span>{" "}
+              <span className="time-caption">{MINUTES_CAPTION}</span>
+            </>
+          )}
+          <span className="time-number">{secondsRemainingAfterMinutes}</span>{" "}
           <span className="time-caption">{SECONDS_CAPTION}</span>
         </span>
       </div>
@@ -60,8 +46,13 @@ function TimeDisplay({ secondsRemaining }: Props) {
     return (
       <div className="time-countdown">
         <span className="time-over">
-          <span className="time-number">-{minutesPastCutoff}</span>{" "}
-          <span className="time-caption">{MINUTES_CAPTION}</span>{" "}
+          <span className="time-number">-</span>
+          {minutesPastCutoff > 0 && (
+            <>
+              <span className="time-number">{minutesPastCutoff}</span>{" "}
+              <span className="time-caption">{MINUTES_CAPTION}</span>{" "}
+            </>
+          )}
           <span className="time-number">{secondsRemainingAfterMinutes}</span>{" "}
           <span className="time-caption">{SECONDS_CAPTION}</span>
         </span>
