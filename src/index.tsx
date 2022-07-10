5df2578f9d673d1de40acfbe47ab8e36d74a9b24
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-07-10 04:31:42 -0700

    Provide service worker to enable offline use

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,3 +12,22 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({
+  // Force updates to service worker to be installed immediately
+  // (The page will reload.)
+  // via https://github.com/facebook/create-react-app/issues/5316#issuecomment-496292914 and https://stackoverflow.com/q/52904430
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener("statechange", (event) => {
+        const target = event.target as ServiceWorker;
+        if (target.state === "activated") {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+    }
+  },
+});
